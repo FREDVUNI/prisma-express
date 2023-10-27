f9df78e33267ee1eda9ae05df6dc25ad7686a303
Fred Vuni <fredvuni809@gmail.com>
2023-10-27 09:21:19 +0300

    refactor: add validation for user exists

--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,16 @@ const createUser = async (req, res) => {
   try {
     const { username, password } = req.body;
 
+    const user = await prisma.author.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (!user)
+      return res.status(403).json({
+        error: "User already exists.",
+      });
+
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
 
@@ -43,16 +53,16 @@ const signIn = async (req, res) => {
       return res.status(404).json({
         message: "wrong email password combination",
       });
-    const user_password = bcrypt.compareSync(password,user.password);
+    const user_password = bcrypt.compareSync(password, user.password);
     if (!user_password)
       return res.status(404).json({
         message: "wrong email password combination",
       });
 
-      res.status(200).json({
-        message: "User has been logged in.",
-        token: JWT.sign({ user: user }, process.env.SECRET_KEY),
-      });
+    res.status(200).json({
+      message: "User has been logged in.",
+      token: JWT.sign({ user: user }, process.env.SECRET_KEY),
+    });
   } catch (error) {
     res.status(500).json(error.message || "There was a server error.");
   }
