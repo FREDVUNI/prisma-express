072e4749e8bdc314d94053355c11ac1152bce53b
Fred Vuni <fredvuni809@gmail.com>
2023-10-28 09:34:37 +0300

    chore: authenticate user by id route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const {getUsers,createUser,deleteUser,signIn}  = require("../controllers/users")
-// const authenticate = require("../helpers/authenticate") 
+const authenticate = require("../helpers/authenticate") 
 
-router.get("/",getUsers)
-// router.get("/",authenticate,getUsers)
+router.get("/",authenticate,getUsers)
 router.post("/signup",createUser)
 router.post("/signin",signIn)
-router.delete("/:id",deleteUser)
+router.delete("/:id",authenticate,deleteUser)
 
 module.exports = router
